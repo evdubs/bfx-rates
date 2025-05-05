59aaf70eef523e87ca1c24f84d250b934e880acf
evdubs <1124030+evdubs@users.noreply.github.com>
2025-05-05 10:41:03 -1000

    add funding rest etl as the websocket version seems to break randomly

---
+++ server/funding-rest-etl.js
@@ -0,0 +1,91 @@
+const request = require('request')
+const pg = require('pg')
+
+const pg_pool = new pg.Pool()
+
+const currencies = [
+  "ADA",
+  "ALG",
+  "APE",
+  "APT",
+  "ATO",
+  "AVAX",
+  "BCHN",
+  "BTC",
+  "COMP",
+  "DAI",
+  "DOGE",
+  "DOT",
+  "DSH",
+  "EGLD",
+  "EOS",
+  "ETC",
+  "ETH",
+  "ETHW",
+  "EUR",
+  "FIL",
+  "GBP",
+  "IOT",
+  "JPY",
+  "LEO",
+  "LINK",
+  "LTC",
+  "MATIC",
+  "MKR",
+  "NEO",
+  "SHIB",
+  "SOL",
+  "SUSHI",
+  "TRX",
+  "UNI",
+  "USD",
+  "UST",
+  "XAUT",
+  "XLM",
+  "XMR",
+  "XRP",
+  "ZEC",
+  "ZRX"
+]
+
+setInterval(() => {
+  // console.log("Hit an interval")
+
+  currencies.forEach((ccy, idx) => {
+    setTimeout((c) => {
+      // console.log(`Do something with ${c}`)
+      var lends = request.get(`https://api.bitfinex.com/v2/trades/f${c}?limit=1000`,
+        (err, res, body) => {
+          try {
+            JSON.parse(body).forEach((row, idx) => {
+              pg_pool.query(`
+insert into bfx.funding_trade (
+  currency,
+  datetime,
+  sequence_id,
+  amount,
+  rate,
+  period
+) values (
+  $1::text,
+  to_timestamp($2::bigint / 1000),
+  $3::bigint,
+  abs($4::numeric),
+  $5::numeric,
+  $6::smallint
+) on conflict (sequence_id) do nothing;`, [ccy,
+                row[1],
+                row[0],
+                row[2],
+                row[3],
+                row[4]]).
+              // then(r => console.log('Called insert without error')).
+              catch(e => console.log(e.stack))
+            })
+          } catch (err) { // JSON.parse error
+            console.error(err.stack)
+          }
+        })
+    }, 1000 * 10 * idx, ccy)
+  })
+}, 1000 * 60 * 10)
