78c23e92acad177683b271fb096333bc9977aa71
evdubs <1124030+evdubs@users.noreply.github.com>
2025-06-27 13:00:51 -1000

    maybe i need two try/catches if the inner function callback doesn't escape

--- server/funding-rest-etl.js
+++ server/funding-rest-etl.js
@@ -57,36 +57,40 @@ setInterval(() => {
       try {
         var funds = request.get(`https://api-pub.bitfinex.com/v2/trades/f${c}/hist?limit=1000`,
           (err, res, body) => {
-            var parsedBody = JSON.parse(body)
-            if (Array.isArray(parsedBody)) {
-              parsedBody.forEach((row, idx) => {
-                pg_pool.query(`
-insert into bfx.funding_trade (
-  currency,
-  datetime,
-  sequence_id,
-  amount,
-  rate,
-  period
-) values (
-  $1::text,
-  to_timestamp($2::bigint / 1000),
-  $3::bigint,
-  abs($4::numeric),
-  $5::numeric,
-  $6::smallint
-) on conflict (sequence_id) do nothing;`, [ccy,
-                  row[1],
-                  row[0],
-                  row[2],
-                  row[3],
-                  row[4]]).
-                  // then(r => console.log('Called insert without error')).
-                  catch(e => console.log(e.stack))
-              })
+            try {
+              var parsedBody = JSON.parse(body)
+              if (Array.isArray(parsedBody)) {
+                parsedBody.forEach((row, idx) => {
+                  pg_pool.query(`
+  insert into bfx.funding_trade (
+    currency,
+    datetime,
+    sequence_id,
+    amount,
+    rate,
+    period
+  ) values (
+    $1::text,
+    to_timestamp($2::bigint / 1000),
+    $3::bigint,
+    abs($4::numeric),
+    $5::numeric,
+    $6::smallint
+  ) on conflict (sequence_id) do nothing;`, [ccy,
+                    row[1],
+                    row[0],
+                    row[2],
+                    row[3],
+                    row[4]]).
+                    // then(r => console.log('Called insert without error')).
+                    catch(e => console.log(e.stack))
+                })
+              }
+            } catch (err) { // JSON.parse error
+              console.error(err.stack)
             }
           })
-      } catch (err) { // JSON.parse error
+      } catch (err) { // request.get error
         console.error(err.stack)
       }
     }, 1000 * 10 * idx, ccy)
